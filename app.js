3cd7b616d6b33e0db3834afd33d3313df25d5b3d
ywow-ai <masyaqin37@gmail.com>
2025-10-28 15:46:12 +0700

    chore: Added disable x-powered-by middleware (Security: Enhanced security by hiding server information)

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-const express = require('express')
-const mysql = require('mysql');
-const app = express()
-const path = require('path')
-require('dotenv').config();
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const path = require("path");
+require("dotenv").config();
 
 // Import Middleware
-const logger = require('./middleware/logger')
-app.use(logger)
-const connection = require('./middleware/db_connect');
+const logger = require("./middleware/logger");
+app.use(logger);
+const connection = require("./middleware/db_connect");
 
 // Dashboard
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, "public")));
 
-app.get('/app1', (req, res) => {
-  res.send('Hello this Apps 1!')
+app.get("/app1", (req, res) => {
+  res.send("Hello this Apps 1!");
 });
 
-app.get('/app2', (req, res) => {
-  res.send('Hello this App 2!')
+app.get("/app2", (req, res) => {
+  res.send("Hello this App 2!");
 });
 
-app.get('/users', (req, res, next) => {
-  const sql = "SELECT * FROM tb_data ORDER BY id desc"
-  connection.query(sql,(error, fields) => {
+app.get("/users", (req, res, next) => {
+  const sql = "SELECT * FROM tb_data ORDER BY id desc";
+  connection.query(sql, (error, fields) => {
     if (error) {
-      console.log('error', error)
+      console.log("error", error);
     } else {
-      res.send(fields)
+      res.send(fields);
     }
-  })
+  });
 });
 
 app.listen(process.env.APP_PORT, () => {
-  console.log(`Example app listening on port ${process.env.APP_PORT}`)
-})
+  console.log(`Example app listening on port ${process.env.APP_PORT}`);
+});
 
-module.exports = app
+app.disable("x-powered-by");
+
+module.exports = app;
