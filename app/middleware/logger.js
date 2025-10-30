7cad5c2a56c37096f6903086c96c665d2c6dac8f
ywow-ai <masyaqin37@gmail.com>
2025-10-30 10:02:46 +0700

    style: Apply code formatting and content update (Formatting: Consistent code style; Content: Added new paragraph to index.html)

--- app/middleware/logger.js
+++ app/middleware/logger.js
@@ -1,9 +1,11 @@
-const moment = require('moment');
+const moment = require("moment");
 
 // Logging
 const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
-    next();
-}  
+  console.log(
+    `${req.protocol}://${req.get("host")}${req.originalUrl}: ${moment().format()}`,
+  );
+  next();
+};
 
-module.exports = logger
+module.exports = logger;
--- app/testing/app.test.js
+++ app/testing/app.test.js
@@ -1,35 +1,35 @@
-const request = require('supertest');
-const mysql = require('mysql');
-require('dotenv').config();
+const request = require("supertest");
+const mysql = require("mysql");
+require("dotenv").config();
 
-const connection = require('../middleware/db_connect');
-const app = require('../app'); // Replace with the path to your application file
+const connection = require("../middleware/db_connect");
+const app = require("../app"); // Replace with the path to your application file
 
-describe('Unit Test /', () => {
-  it('should respond with index.html', async () => {
-    const response = await request(app).get('/');
+describe("Unit Test /", () => {
+  it("should respond with index.html", async () => {
+    const response = await request(app).get("/");
     expect(response.status).toBe(200);
   });
 });
 
-describe('Unit Test /app1', () => {
-    it('should respond with "Hello App1!"', async () => {
-      const response = await request(app).get('/app1');
-      expect(response.status).toBe(200);
-    });
+describe("Unit Test /app1", () => {
+  it('should respond with "Hello App1!"', async () => {
+    const response = await request(app).get("/app1");
+    expect(response.status).toBe(200);
   });
+});
 
-  describe('Unit Test /app2', () => {
-    it('should respond with "Hello App2!"', async () => {
-      const response = await request(app).get('/app2');
-      expect(response.status).toBe(200);
-    });
+describe("Unit Test /app2", () => {
+  it('should respond with "Hello App2!"', async () => {
+    const response = await request(app).get("/app2");
+    expect(response.status).toBe(200);
   });
+});
 
-describe('Integration Test Connect Database', () => {
+describe("Integration Test Connect Database", () => {
   beforeEach(() => {
     // Set the connection for the application to use
-    app.set('connection', connection.connect);
+    app.set("connection", connection.connect);
   });
 
   afterEach(() => {
@@ -37,8 +37,8 @@ describe('Integration Test Connect Database', () => {
     connection.end();
   });
 
-  it('should respond with users data', async () => {
-    const response = await request(app).get('/users');
+  it("should respond with users data", async () => {
+    const response = await request(app).get("/users");
     expect(response.status).toBe(200);
   });
 });
